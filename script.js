eaa1c76f6e37a48ac6f87bb1bec02f3aa5a65b8b
Teresa Febiani Krismardi <febi60299@gmail.com>
2026-02-12 22:56:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,17 @@
 const navbarNav = document.querySelector(".navbar-nav");
 
 //ketika hamburger menu di klik
-document.querySelector("hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
 //Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("hamburger-menu");
+const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
+
 
